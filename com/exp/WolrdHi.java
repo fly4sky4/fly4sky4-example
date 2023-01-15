6dfb628dbfc33a19c019ac455c114f0dc2f47dac
fly4sky4 <fly4sky4@gmail.com>
2023-01-15 20:44:11 +0800

    commit WorldHi class

---
+++ com/exp/WolrdHi.java
@@ -0,0 +1,9 @@
+package com.exp;
+
+public class WolrdHi {
+    private static final String msg = "hello hi";
+
+    public static void main(String[] args) {
+        System.out.println(msg);
+    }
+}
