e3eef3a33eb4b74f6b0efbe811d9410b35c84f58
fly4sky4 <fly4sky4@gmail.com>
2023-01-16 21:21:24 +0800

    commit addExample

---
+++ com/exp/AddExample.java
@@ -0,0 +1,13 @@
+package com.exp;
+
+public class AddExample {
+    public static int add (int m, int n) {
+        return m + n;
+    }
+
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 100;
+        System.out.println(add(a, b));
+    }
+}
