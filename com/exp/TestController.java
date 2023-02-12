7b50fd6b62270956030f4407fc4387afc7315dec
fly4sky4 <fly4sky4@gmail.com>
2023-02-12 13:33:34 +0800

    commit addExample

---
+++ com/exp/TestController.java
@@ -0,0 +1,14 @@
+package com.exp;
+
+public class TestController {
+
+        public static void main(String[] args) {
+            String str = "string abc touppercase123 ";
+            String strUpper = str.toUpperCase();
+            System.out.println("Original String: " + str);
+            System.out.println("String changed to upper case: "
+                    + strUpper);
+        }
+
+
+}
